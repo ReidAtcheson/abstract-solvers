e8a1e58062445a897b1f6bd6795118ffbc0366d8
Reid Atcheson <reid.atcheson@gmail.com>
2017-05-07 15:12:31 -0500

    abstract example workign

--- examples/FunctionHilbert.ml
+++ examples/FunctionHilbert.ml
@@ -4,63 +4,16 @@ open Hilbert
 
 module Gquad = Gk.Quad (struct let maxit=100 end)
 (** Some helper functions .*)
-let cheb n x = cos ((float_of_int n) *. (acos x) )
-let chebf n x  = 
-  let nf = float_of_int n in
-  let theta = acos x in
-  sin ((nf +. 1.0) *. theta ) /. (sin theta)
-let dcheb n x = 
-  let nf = float_of_int n in
-  nf *. chebf (n-1) x
-
-let polyval cs x = 
-  let n = length cs in
-  let out = ref 0.0 in
-  for i = 0 to (n-1) do
-    out := !out +. cs.(i) *. (cheb i x);
-  done;
-  !out
-
-let cheb_coeff i f =
-  let tol = 1e-6 in
-  let chebnorm2 = Gquad.gkint (fun x -> (cheb i x) *. (cheb i x) ) (-1.0) (1.0) tol in
-  let chebnorm = sqrt chebnorm2 in
-  let integrand x = (f x) *. (cheb i) 1.0 /. ( sqrt (1.0 -. x *. x)) in
-  let c = Gquad.gkint integrand (-1.0) (1.0) tol in
-  c /. chebnorm
-
-
-let cheb_exp f = 
-  let maxit = 10 in
-  let i = ref 0 in
-  let tol = 1e-6 in
-  let err = ref 1.0 in
-  let cs = ref (Array.init 1 (fun x -> 1.0)) in
-  while !i < maxit && !err > tol do
-    i := !i + 1;
-    cs := Array.init !i (fun j -> cheb_coeff j f);     
-    let cf = polyval !cs in
-    let interr x = ((cf x) -. (f x)) *. ( (cf x) -. (f x)) in
-    let err2 = Gquad.gkint interr (-1.0) (1.0) tol in
-    err := sqrt err2;
-  done;
-  !cs
-
 (** Space of functions *)
 
-(** Function is either Ocaml function or 
- * Chebyshev expansion of Ocaml function*)
-type aproxfn = 
-    Fn of (float -> float)
-  | Cheb of (float array)
 
 module FunctionHilbert : 
   HilbertSpace 
-    with type v = aproxfn
+    with type v = float -> float
     with type r = float
     with type c = float
   = struct
-  type v = aproxfn
+  type v = float -> float
   type r = float
   type c = float
 
@@ -86,60 +39,17 @@ module FunctionHilbert :
   let sqrt x = sqrt x
 
 
-  let len v = match v with
-    Fn f -> 0
-  | Cheb cs -> Array.length cs
-  let getexp v = match v with
-    Fn f -> Array.make 1 0.0
-  | Cheb cs -> cs
-  let expand v = match v with
-     Fn f -> Cheb (cheb_exp f)
-    |Cheb cs -> Cheb cs
     
-  let addv v1 v2 =
-    let cv1 = expand v1 in
-    let cv2 = expand v2 in
-    let cs1 = getexp cv1 in
-    let cs2 = getexp cv2 in
-    let pv1 = polyval cs1 in
-    let pv2 = polyval cs2 in
-    let p = fun x -> (pv1 x) +. (pv2 x) in
-    Cheb (cheb_exp p)
+  let addv v1 v2 = fun x -> (v1 x) +. (v2 x)
 
-  let subv v1 v2 = 
-    let cv1 = expand v1 in
-    let cv2 = expand v2 in
-    let cs1 = getexp cv1 in
-    let cs2 = getexp cv2 in
-    let pv1 = polyval cs1 in
-    let pv2 = polyval cs2 in
-    let p = fun x -> (pv1 x) -. (pv2 x) in
-    Cheb (cheb_exp p)
+  let subv v1 v2 = fun x -> (v1 x) -. (v2 x)
 
-  let mulv a v   =
-    let cv = expand v in
-    let cs = getexp cv in
-    Cheb (Array.init (Array.length cs) (
-      fun i -> (cs.(i) *.  a)
-    ))
-
+  let mulv a v   = fun x -> a *. (v x)
     
-  let divv v a   =  
-    let cv = expand v in
-    let cs = getexp cv in
-    Cheb (Array.init (Array.length cs) (
-      fun i -> (cs.(i) /.  a)
-    ))
-
+  let divv v a   = fun x -> (v x) /. a
   let dot v1 v2 = 
-    let cv1 = expand v1 in
-    let cv2 = expand v2 in
-    let cs1 = getexp cv1 in
-    let cs2 = getexp cv2 in
-    let pv1 = polyval cs1 in
-    let pv2 = polyval cs2 in
-    let p = fun x -> (pv1 x) *. (pv2 x) in
-    Gquad.gkint p left right tol
+    let v = fun x -> (v1 x) *. (v2 x) in
+    Gquad.gkint v left right tol
 
   let norm v = sqrt (dot v v)
 end;;
--- tests/test_abstractness.ml
+++ tests/test_abstractness.ml
@@ -7,21 +7,15 @@ module Q = Params (FH)
 module R  = ConjugateGradient (FH)
 
 
-
-let diff f x = 
-  let h = 1e-8 in
-  let fp1 = if abs_float (x +.h -. 1.0) < h then 0.0 else f (x +. h) in
-  let fm1 = if abs_float (x -.h +. 1.0) < h then 0.0 else f (x +. h) in
-  let diff_f = fp1 -. fm1 in
-  diff_f /. (2.0 *. h)
+let modulate f  = 
+  fun x -> -. (x -. 1.0) *. (x +. 1.0) *. (f x)
 
 
-let a f = 
-  diff f
+let a f = modulate f
 
-let init = Fn (fun x -> 1.0)
-let b = Fn (fun x -> 1.0)
-let p  = { Q.init=init;maxit=2;reltol=1e-6;check_gap=(1);}
+let init =  (fun x -> 1.0)
+let b =  (fun x -> 1.0)
+let p  = {Q.init=init;maxit=15;reltol=1e-6;check_gap=(1);}
 let soln = R.solve p a b
 
 
